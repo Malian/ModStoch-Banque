65f33bf8f21162ac06f4bb37cc8f6f37a2f376f6
Malian De Ron <deronmalian@wifi-studentl-6999.sri.ucl.ac.be>
2015-03-10 10:05:37 +0100

    Better images

--- matlab/output_results.m
+++ matlab/output_results.m
@@ -8,7 +8,7 @@ images_dir = '../report/images/';
 
 % List all wanted files
 served_files = dir([data_dir '*clients_served.csv']);
-day = {'Monday PM', 'Tuesday PM', 'Wensday AM', 'Wednesday AM', 'Wednesday PM', 'Friday AM'};
+day = {'Monday PM', 'Tuesday PM', 'Wednesday AM', 'Wednesday PM', 'Friday AM'};
 
 % Plot and save figures
 for i = 1:length(served_files)
@@ -19,20 +19,19 @@ for i = 1:length(served_files)
 	createPlot(i, time, CA(i,:), 1);
 	createPlot(i, time, served, 2);
     
-	carac_str = strsplit(served_files_name,'clients_served.csv');
-	carac_str = carac_str(1,1){1};
+	%carac_str = strsplit(served_files_name,'clients_served.csv');
+	%carac_str = carac_str(1,1){1};
 	
-    pause(1);
-	outputfig = strcat(images_dir,carac_str, 'clients_served.eps');
-	print(i,outputfig, '-depsc');
+	%outputfig = strcat(images_dir,carac_str, 'clients_served.eps');
+	%print(i,outputfig, '-depsc');
 end
 
-c = csvread(strcat(data_dir,'half_day_opt_capacity.csv'));
-fid = fopen('../report/tex_matlab/c_value.tex','w');
-fprintf(fid,'$ %g $',c(1,1));
-fclose(fid);
+%c = csvread(strcat(data_dir,'half_day_opt_capacity.csv'));
+%fid = fopen('../report/tex_matlab/c_value.tex','w');
+%fprintf(fid,'$ %g $',c(1,1));
+%fclose(fid);
 
-Nt = ceil(c(1,1)*mean_processing_time);
-fid = fopen('../report/tex_matlab/tellers.tex','w');
-fprintf(fid,'$ %g $',Nt);
-fclose(fid);
+%Nt = ceil(c(1,1)*mean_processing_time);
+%fid = fopen('../report/tex_matlab/tellers.tex','w');
+%fprintf(fid,'$ %g $',Nt);
+%fclose(fid);
